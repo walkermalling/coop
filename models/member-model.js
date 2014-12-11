404513e66d5a0bd011400acfba043f9326ab4a33
Walker <walker.malling@gmail.com>
2014-12-11 02:33:55 -0800

    Add script to generate recommendations
    Script uses the Slope One algorithm, although the averages are not weighted.

--- models/member-model.js
+++ models/member-model.js
@@ -7,7 +7,9 @@ var MemberSchema = mongoose.Schema({
   stats: {
     points: {type:Number, default:5},
     providerInstances: {type:Number, default:0},
-    receiverInstances: {type:Number, default:0}
+    receiverInstances: {type:Number, default:0},
+    ratings: {type:mongoose.Schema.Types.Mixed, default:''},
+    recommendations: {type:mongoose.Schema.Types.Mixed, default:''}
   },
   updatedAt: {type:Date, default:Date.now},
   createdAt: {type:Date, default:Date.now}
